f71fe5abcf140225cf5ec29281fdd804b666e66e
sanathkumarbs <b.s.sanathkumar@gmail.com>
2018-06-27 21:46:38 -0700

    doubly linked list implementation

--- linkedlists/linkedlist.py
+++ linkedlists/linkedlist.py
@@ -9,6 +9,17 @@ class Node(object):
         self.data = data
         self.next = next
 
+
+class DoubleNode(object):
+    """Implementation of a Node of a doubly linkedlist."""
+
+    def __init__(self, data, prev = None, next = None):
+        """Initialize a node with data, prev and next pointers."""
+        self.data = data
+        self.prev = prev
+        self.next = next
+
+
 class LinkedList(object):
     """Implementation of a 0th indexed linkedlist."""
 
@@ -95,4 +106,92 @@ class LinkedList(object):
                 cur = cur.next
             
             cur.next = None
+
+
+class DoublyLinkedList(LinkedList):
+    """Implementation of a 0th indexed doubly linkedlist."""
+
+    def __init__(self, head = None):
+        LinkedList.__init__(self, head)
+
+    def push(self, data, index = None):
+        doublenode = DoubleNode(data)
+
+        if index is not None:
+            # push at the index
+            if index == 0 and self.head:
+                # insert at the head
+                # head node exists
+                doublenode.next = self.head
+                self.head.prev = doublenode
+                self.head = doublenode
+            elif index == 0 and not self.head:
+                # insert at the head
+                # head node doesn't exist
+                self.head = doublenode
+            elif index > 0 and index <= self.length() and self.head:
+                # insert at the index
+                # head node exists
+                cur = self.head
+                prev = self.head.prev
+
+                for _ in range(0, index):
+                    prev = cur
+                    cur = cur.next
+
+                prev.next = doublenode
+                doublenode.prev = prev
+
+                doublenode.next = cur
+                cur.prev = doublenode
+            else:
+                raise KeyError("Invalid index. Expected {0} to {1}".format(0, self.length()))
+        else:
+            # push at the end
+            if self.head:
+                # head node exists
+                cur = self.head
+                prev = self.head.prev
+
+                while cur is not None:
+                    prev = cur
+                    cur = cur.next    
+
+                prev.next = doublenode
+                doublenode.prev = prev
+            else:
+                # create head node
+                self.head = doublenode
+
+    def pop(self, index = Node):
+        if index is not None:
+            # remove the index node
+            if index == 0 and self.head:
+                # remove the head 
+                self.head = self.head.next
+                self.head.prev = None
+            elif index > 0 and index < self.length() and self.head:
+                # remove the index node
+                cur = self.head
+                prev = self.head.prev
+
+                for _ in range(0, index):
+                    prev = cur
+                    cur = cur.next
+
+                prev.next = cur.next
+                if cur.next:
+                    cur.next.prev = prev
+        else:
+            # remove the last node
+            if self.head:
+                # head exists
+                cur = self.head
+                prev = self.head.prev
+
+                while cur is not None:
+                    prev = cur
+                    cur = cur.next
+
+                prev.prev.next = None
     
--- linkedlists/tests/test_basic_functionality.py
+++ linkedlists/tests/test_basic_functionality.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import logging
 from linkedlists.linkedlist import Node
-from linkedlists.linkedlist import LinkedList
+from linkedlists.linkedlist import LinkedList, DoublyLinkedList
 
 logging.basicConfig(level=logging.DEBUG)
 
@@ -56,6 +56,7 @@ class TestLinkedList(object):
         
         logging.debug("Data is {0}".format(ll.head.data))
         assert(ll.head.data == data)
+        assert(ll.head.next.data == 0)
 
     def test_three(self, size = 4):
         """Testing get on linkedlist."""
@@ -97,3 +98,122 @@ class TestLinkedList(object):
         ll.pop(index)
         logging.debug("Data is {0}".format(ll.get(index).data))
         assert(ll.get(index).data == 3)
+
+
+class TestDoublyLinkedList(object):
+
+    def _get_sample_linkedlist(self, size = 4):
+        """Create a sample linkedlist and return the doubly linkedlist obj"""
+        ll = DoublyLinkedList()
+
+        for item in range(size):
+            ll.push(item)
+        
+        return ll
+
+    def test_one(self, size = 4):
+        """Testing traversing a doubly linkedlist (forward)."""
+        ll = self._get_sample_linkedlist(size)
+        cur = ll.head
+        count = 0
+        
+        while cur:
+            count += 1
+            logging.debug("Data is {0}".format(cur.data))
+            cur = cur.next
+
+        assert(count == 4)
+
+    def test_two(self, size = 4):
+        """Testing traversing a doubly linkedlist (backward)."""
+        ll = self._get_sample_linkedlist(size)
+
+        cur = ll.head
+        prev = ll.head.prev
+
+        count = 0
+        
+        while cur:
+            count += 1
+            logging.debug("Data is {0}".format(cur.data))
+            prev = cur
+            cur = cur.next
+        
+        cur = prev
+
+        while cur:
+            count -= 1
+            logging.debug("Data is {0}".format(cur.data))
+            cur = cur.prev
+
+        assert(count == 0)
+    
+    def test_three(self, data = 20):
+        """Testing inserting node at the head."""
+        ll = self._get_sample_linkedlist()
+        ll.push(data, 0)
+        
+        logging.debug("Data is {0}".format(ll.head.data))
+        assert(ll.head.data == data)
+        assert(ll.head.next.data == 0)
+        assert(ll.head.next.prev.data == data)
+        assert(ll.head.prev == None)
+
+    def test_four(self, size = 4):
+        """Testing get on linkedlist."""
+        ll = self._get_sample_linkedlist(size)
+        assert(ll.get(index = 1).data == 1)
+
+    def test_five(self, index = 2, data = 50):
+        """Testing inseting node at nth position."""
+        ll = self._get_sample_linkedlist(size = 10)
+        
+        nextval = ll.get(index).data
+        prevval = ll.get(index-1).data
+
+        ll.push(data, index)
+
+        assert(ll.get(index).data == data)
+        assert(ll.get(index).next.data == nextval)
+        assert(ll.get(index).next.prev.data == data)
+        assert(ll.get(index).prev.data == prevval)
+
+    def test_six(self):
+        """Testing deleting head node."""
+        ll = self._get_sample_linkedlist()
+        logging.debug("Data is {0}".format(ll.head.data))
+
+        ll.pop(0)
+        logging.debug("Data is {0}".format(ll.head.data))
+        assert(ll.head.data == 1)
+        assert(ll.head.prev == None)
+        assert(ll.head.next.data == 2)
+        assert(ll.head.next.prev.data == 1)
+
+    def test_seven(self):
+        """Testing deleting the last node."""
+        ll = self._get_sample_linkedlist()
+        last = ll.length() - 1
+        logging.debug("Data is {0}".format(ll.get(last).data))
+
+        ll.pop(last)
+        last = ll.length() - 1
+        logging.debug("Data is {0}".format(ll.get(last).data))
+ 
+        assert(ll.get(last).data == 2)
+
+    def test_eight(self, index = 1):
+        """Testing deleting a nth node."""
+        ll = self._get_sample_linkedlist()
+        logging.debug("Data is {0}".format(ll.get(index).data))
+
+        nextval = ll.get(index + 1).next.data
+        prevval = ll.get(index - 1).data
+
+        ll.pop(index)
+        logging.debug("Data is {0}".format(ll.get(index).data))
+
+        assert(ll.get(index).data == 2)
+        assert(ll.get(index).next.data == nextval)
+        assert(ll.get(index).prev.data == prevval)
+        assert(ll.get(index).next.prev.data == 2)
